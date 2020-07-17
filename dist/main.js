155226abee9c640acc532565ade6423ae5bbe39d
Erin Kraemer <erinbkraemer@gmail.com>
2020-07-17 13:41:53 -0700

    a farewell to the trash panda and fixed box alignment

--- dist/main.js
+++ dist/main.js
@@ -132,6 +132,7 @@ module.exports = Util;
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
+// ESM COMPAT FLAG
 __webpack_require__.r(__webpack_exports__);
 
 // CONCATENATED MODULE: ./src/physics.js
