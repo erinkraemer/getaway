911034c311c914efe6fa1e49aa17960edb31948b
Erin Kraemer <erinbkraemer@gmail.com>
2020-07-21 13:10:28 -0700

    Added blue and green object box, blue default, no interaction change yet

--- dist/main.js
+++ dist/main.js
@@ -276,6 +276,10 @@ const lifeImg = new Image();
 const scale = 1.5;
 const redBox = new Image();
 redBox.src = "./assets/images/redbox.png";
+const blueBox = new Image();
+blueBox.src = "./assets/images/bluebox.png";
+const greenBox = new Image();
+greenBox.src = "./assets/images/greenbox.png";
 
 class life_Life {
   constructor(physics, imgSrc, marked, assetid,distance = -1) {
@@ -284,7 +288,7 @@ class life_Life {
     lifeImg.src = imgSrc;
 
     this.sprite = new src_sprite(lifeImg, 50, 50, 1.0*scale, 1.0*scale);
-    this.box = new src_sprite(redBox, 40, 50, 1.0*scale, 1.3*scale)
+    this.box = new src_sprite(blueBox, 40, 50, 1.0*scale, 1.3*scale)
     this.marked = marked;
     this.assetid = assetid;
     this.recognizedType = this.marked? "U" : assetid[0];  
@@ -316,16 +320,20 @@ var util_default = /*#__PURE__*/__webpack_require__.n(util);
 
 
 const rockImg = new Image();
-const obstacle_redBox = new Image();
 const obstacle_scale = 1.5;
 //rockImg.src = "./assets/images/rock.png";
-obstacle_redBox.src = "./assets/images/redbox.png"; //TODO_ERIN: Convert it to a bluebox
+const obstacle_redBox = new Image(); //DONE: Convert it to a bluebox
+obstacle_redBox.src = "./assets/images/redbox.png";
+const obstacle_blueBox = new Image();
+obstacle_blueBox.src = "./assets/images/bluebox.png";
+const obstacle_greenBox = new Image();
+obstacle_greenBox.src = "./assets/images/greenbox.png"; 
 class obstacle_Obstacle {
   constructor(physics,imgSrc,marked,assetid, distance=-1) {
     this.physics = physics;
     rockImg.src = imgSrc;
     this.sprite = new src_sprite(rockImg, 50, 50, 1.0 * obstacle_scale, 1.0 * obstacle_scale);
-    this.box = new src_sprite(obstacle_redBox, 40, 50, 1.0 * obstacle_scale, 1.3 * obstacle_scale)
+    this.box = new src_sprite(obstacle_blueBox, 40, 50, 1.0 * obstacle_scale, 1.3 * obstacle_scale)
     this.marked = marked;
     this.assetid = assetid;
     this.recognizedType = this.marked? "U" : assetid[0];  
@@ -357,13 +365,17 @@ const cashImg = new Image();
 const cash_scale = 1.5;
 const cash_redBox = new Image();
 cash_redBox.src = "./assets/images/redbox.png";
+const cash_blueBox = new Image();
+cash_blueBox.src = "./assets/images/bluebox.png";
+const cash_greenBox = new Image();
+cash_greenBox.src = "./assets/images/greenbox.png";
 class cash_Cash {
   constructor(physics, imgSrc, marked, assetid, distance = -1) {
     
     this.physics = physics;
     cashImg.src = imgSrc;
     this.sprite = new src_sprite(cashImg, 50, 50, 1.0 * cash_scale, 1.0 * cash_scale);
-    this.box = new src_sprite(cash_redBox, 40, 50, 1 * cash_scale, 1.3 * cash_scale)
+    this.box = new src_sprite(cash_blueBox, 40, 50, 1 * cash_scale, 1.3 * cash_scale)
     this.marked = marked;
     this.assetid = assetid;
     this.distance = distance;
@@ -641,7 +653,26 @@ class game_Game {
       clearInterval(y);
     }, blinkDuration, interval);
   }
-  
+/*
+  // Added to change color of box around sprites
+  holdObjectBox(blinkDuration,color, correct, object) {
+    var interval = window.setInterval(function () {
+      if correct {
+
+      }
+      else {
+
+      }
+      this.
+      document.getElementById("canvas").style["border"] = "20px solid "+color;
+    }, 5);
+
+    setTimeout(function (y) {
+      document.getElementById("canvas").style["border"] = "20px solid black";
+      clearInterval(y);
+    }, blinkDuration, interval);
+  } 
+ */ 
   holdDistractorCanvas(blinkDuration, color) {
     var interval = window.setInterval(function () {
       document.getElementById("distractortask").style["border"] = "20px solid " + color;
@@ -805,6 +836,7 @@ setRecognizedType(assetid,assetUserSpecifiedType){
               //Blink green
             //document.getElementById("canvas").style["border"] = "20px solid green";
             this.holdCanvas(2000, "green");
+            console.log(this.life);
             // Write functions to do whatever has to be done when user enteres correct response
             this.activeResponse = false;
             this.setRecognizedType(this.boxed[0], i);
@@ -1841,6 +1873,10 @@ const setupControlListeners = (game) => {
 
 const redBoxImg = new Image();
 redBoxImg.src = "./assets/images/redbox.png";
+const blueBoxImg = new Image();
+blueBoxImg.src = "./assets/images/bluebox.png";
+const greenBoxImg = new Image();
+greenBoxImg.src = "./assets/images/greenbox.png";
 const bumblebee_lifeImgFolder = "./assets/images/life/";
 const bumblebee_obstacleImgFolder = "./assets/images/obstacle/";
 const bumblebee_moneyImgFolder = "./assets/images/money/";
