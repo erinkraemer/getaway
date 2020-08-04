2a48f9e750102592efbff7f3cd5faa63eb3952bf
Erin Kraemer <erinbkraemer@gmail.com>
2020-08-04 13:53:40 -0700

    forgot to npx

--- static/js/dist/main.js
+++ static/js/dist/main.js
@@ -540,7 +540,7 @@ const CONTROLLER_REACTION_TIME = 2000;
 const CONTROLLER_SAMPLING_TIME = 500;// in milliseconds
 const DISTRACTOR_TASK_TIME = 5000; //Also the timeout for distractor tasl // in milliseconds
 const DISTRACTOR_TASK_PAUSE = 1500;// in milliseconds
-const GAME_TIME = 600000;// 10 minutes in milliseconds
+const GAME_TIME = 10;// 10 minutes in milliseconds
 
 const MIN_RES_WIDTH = 1280;
 const MIN_RES_HEIGHT = 800;
