aee0e433e3f227ac886a439488d17cd3557371da
Erin Kraemer <erinbkraemer@gmail.com>
2021-01-18 22:36:15 -0800

    change compute bonus to remove indentation error

--- custom.py
+++ custom.py
@@ -116,18 +116,27 @@ def compute_bonus(fraction_primary, fraction_distractor):
         raise ExperimentError('improper_inputs')  # i don't like returning HTML to JSON requests...  maybe should change this
     uniqueId = request.args['uniqueId']
 
-    try: 
+    #try: 
         # lookup user in database
-        user = Participant.query.\
-               filter(Participant.uniqueid == uniqueId).\
-               one()
-        user_data = loads(user.datastring) # load datastring from JSON
-        bonus = 0
+        # user = Participant.query.\
+        #        filter(Participant.uniqueid == uniqueId).\
+        #        one()
+        # user_data = loads(user.datastring) # load datastring from JSON
+        # bonus = 0
 
-        for record in user_data['data']: # for line in data file
-            trial = record['trialdata']
-            if trial['phase']=='TEST':
-                if trial['hit']==True:
+        # for record in user_data['data']: # for line in data file
+        #     trial = record['trialdata']
+        #     if trial['phase']=='TEST':
+        #         if trial['hit']==True:
+
+    #     user.bonus = bonus
+    #     db_session.add(user)
+    #     db_session.commit()
+    #     resp = {"bonusComputed": "success"}
+    #     return jsonify(**resp)
+    # except:
+    #     abort(404)  # again, bad to display HTML, but...
+                    
         # bonus = a + b*exp(-C*cumulative_x), 
         # where: cumulative_x = alpha*fraction_primary + (1-alpha)*fraction_distractor, 
         # and alpha>0.5 (must be <1) means we weigh the primary queries more; 
@@ -146,11 +155,5 @@ def compute_bonus(fraction_primary, fraction_distractor):
         # cumulative_x = alpha * fraction_primary + (1 - alpha) * fraction_distractor
         # bonus = a + b * math.exp(-C * cumulative_x)
 
-        user.bonus = bonus
-        db_session.add(user)
-        db_session.commit()
-        resp = {"bonusComputed": "success"}
-        return jsonify(**resp)
-    except:
-        abort(404)  # again, bad to display HTML, but...
+        
 
